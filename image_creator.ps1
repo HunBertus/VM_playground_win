e0fb19a7de191f116a355cdcde061abb9442f32d
HunBertus <szbertus@gmail.com>
2020-04-09 22:42:11 +0200

    first commit

---
+++ image_creator.ps1
@@ -0,0 +1,16 @@
+﻿function create_vm {
+    param ( [string]$name )
+new-vm -Name $name -MemoryStartupBytes 4Gb -Generation 2 `
+-NewVHDPath C:\Hyper-V\$name.vhdx -NewVHDSizeBytes 25Gb `
+-SwitchName Privat01
+Add-VMDvdDrive -VMName $name
+$bootorder = Get-VMFirmware -VMName $name
+$hdddrive = $bootorder.BootOrder[0]
+$pxe = $bootorder.BootOrder[1]
+$dvddrive = $bootorder.BootOrder[2]
+Set-VMFirmware -VMName $name -BootOrder $dvddrive,$hdddrive,$pxe
+}
+
+# 1. VM
+create_vm -name unattended_master_image_de
+Set-VMDvdDrive -VMName unattended_master_image_de -Path C:\ISO_files\17763.1.180914-1434.rs5_release_SERVER_EVAL_x64FRE_de-de.iso
---
+++ vm_to_boot.ps1
@@ -0,0 +1,36 @@
+﻿# funktion to make a vm from a generalized master vhdx
+# syntax create_vm <vmname>
+
+function create_vm {
+    param ( [string]$vmname )
+
+# create a home directory for the vm
+New-Item -Path "C:\Hyper-V\" -Name "$vmname" -ItemType "directory"
+
+# copying master to vm's home
+Copy-Item C:\Hyper-V\master_image_de.vhdx -Destination C:\Hyper-V\$vmname
+
+# creating a new vm (maybe later could be parametrized ram, generation, etc)
+new-vm `
+-Name $vmname `
+-MemoryStartupBytes 4Gb `
+-Generation 2 `
+-VHDPath C:\Hyper-V\$vmname\master_image_de.vhdx `
+-SwitchName "Default Switch"
+
+# setting bootorder
+$bootorder = Get-VMFirmware -VMName $vmname
+$hdddrive = $bootorder.BootOrder[0]
+$pxe = $bootorder.BootOrder[1]
+Set-VMFirmware -VMName $vmname -BootOrder $hdddrive,$pxe
+
+# starting vm
+Start-VM -VMName $vmname
+}
+# reading credentials
+$username = "administrator"
+$password = ConvertTo-SecureString "123user!" -AsPlainText -Force
+$cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $username, $password
+
+# 1. VM
+create_vm testvm1
